b90f314624c7327f8381e574c0c63a5ccf54cdbf
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-02-08 15:17:10 +0100

    Read stations
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import csv
+import zipfile
 import json
 from io import TextIOWrapper
 from dataclasses import dataclass, is_dataclass, asdict
@@ -15,13 +17,23 @@ NUM_EMPLOYEES = 500
 
 @dataclass
 class Customer:
+    id: int
     first_name: str
     last_name: str
 
 @dataclass
 class Station:
+    id: int
     name: str
     station_type: int
+
+@dataclass
+class Connection:
+    pass
+
+@dataclass
+class Ticket:
+    pass
 
 @dataclass
 class Employee:
@@ -30,12 +42,15 @@ class Employee:
 
 customers = []
 employees = []
+stations = []
 
 # download and prepare data
 
 res = requests.get(f'https://randomuser.me/api?results={NUM_CUSTOMERS}')
+customer_id = 1
 for cust in res.json()['results']:
-    customers.append(Customer(cust['name']['first'], cust['name']['last']))
+    customers.append(Customer(customer_id, cust['name']['first'], cust['name']['last']))
+    customer_id += 1
 
 res = requests.get(f'https://randomuser.me/api?results={NUM_EMPLOYEES}')
 seen_usernames = set()
@@ -50,12 +65,22 @@ for emp in res.json()['results']:
 
 GTFS_ZIP = 'gtfs.zip'
 if not os.path.isfile(GTFS_ZIP):
-    res = requests.get('https://opentransportdata.swiss/dataset/1aff176a-9665-4395-a3b1-03e3032a0373/resource/f587c0fb-e410-4fd2-a468-4f6c4c40a049/download/gtfs_fp2021_2021-12-08_09-10.zip')
+    res = requests.get('https://opentransportdata.swiss/dataset/00811070-1b51-43da-87af-b1901e906323/resource/8fd78d4f-b63e-4f00-88a1-427207a7cc46/download/gtfs_fp2022_2022-02-02_17-26.zip')
     with open(GTFS_ZIP, 'wb') as f: 
         for chunk in res.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
 
+archive = zipfile.ZipFile(GTFS_ZIP)
+stops = archive.open('stops.txt')
+_ = stops.readline() # header
+while (stop := stops.readline()):
+    stopline = stop.decode()
+    r = csv.reader([stopline])
+    values = list(r)[0]
+    if values[0].isdigit():
+        # TODO:
+        stations.append(Station(int(values[0]), values[1], 123))
 
 
 # dump
@@ -68,10 +93,25 @@ class EnhancedJSONEncoder(json.JSONEncoder):
 with open('data.json', 'w') as f:
     f.write(json.dumps({
         'customers': customers,
-        'employees': employees
+        'employees': employees,
+        'stations': stations
     }, cls=EnhancedJSONEncoder, indent=2))
 
 # create sql files
+
+def write_stations(sql: TextIOWrapper):
+    sql.writelines([
+        '''create table Station(
+                Id integer primary key,
+                Name varchar(100) not null,
+                TransportType int not null);\n\n'''
+    ])
+
+    for station in stations:
+        sql.writelines([
+            f'insert into Station(Id, Name, TransportType) values ({station.id}, \'{station.name}\', {station.station_type});\n'
+        ])
+    
 
 def write_employees(sql: TextIOWrapper):
     sql.writelines([
@@ -87,7 +127,6 @@ def write_employees(sql: TextIOWrapper):
 
 
 def write_customers(sql: TextIOWrapper):
-    customer_id = 1
     sql.writelines([
         '''create table Customer(
                 Id integer not null primary key,
@@ -97,9 +136,8 @@ def write_customers(sql: TextIOWrapper):
 
     for customer in customers:
         sql.writelines([
-            f'insert into Customer(Id, FirstName, LastName) values ({customer_id}, \'{customer.first_name}\', \'{customer.last_name}\');\n'
+            f'insert into Customer(Id, FirstName, LastName) values ({customer.id}, \'{customer.first_name}\', \'{customer.last_name}\');\n'
         ])
-        customer_id += 1
 
 with open(MSSQL_OUT, 'w') as mssql:
     with open(MYSQL_OUT, 'w') as mysql:
@@ -111,3 +149,9 @@ with open(MSSQL_OUT, 'w') as mssql:
 
         write_employees(mssql)
         write_employees(mysql)
+
+        mssql.write('\n\n')
+        mysql.write('\n\n')
+
+        write_stations(mssql)
+        write_stations(mysql)
